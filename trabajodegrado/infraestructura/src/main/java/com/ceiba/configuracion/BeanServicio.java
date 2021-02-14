dca466b74d2bb383e70928393095b84741c7ba54
Miguel Angel Osorio Giron - Ceiba Software <miguel.osorio@ceiba.com.co>
2021-02-13 21:27:41 -0500

    Creación de tablas y creacópn de los beans para los servicios del dominio

--- trabajodegrado/infraestructura/src/main/java/com/ceiba/configuracion/BeanServicio.java
+++ trabajodegrado/infraestructura/src/main/java/com/ceiba/configuracion/BeanServicio.java
@@ -1,5 +1,11 @@
 package com.ceiba.configuracion;
 
+import com.ceiba.cita.puerto.repositorio.RepositorioCita;
+import com.ceiba.cita.servicio.ServicioCrearCita;
+import com.ceiba.trabajodegrado.puerto.repositorio.RepositorioTrabajoDeGrado;
+import com.ceiba.trabajodegrado.servicio.ServicioActualizarTrabajoDeGrado;
+import com.ceiba.trabajodegrado.servicio.ServicioCrearTrabajoDeGrado;
+import com.ceiba.trabajodegrado.servicio.ServicioEliminarTrabajoDeGrado;
 import com.ceiba.usuario.puerto.repositorio.RepositorioUsuario;
 import com.ceiba.usuario.servicio.ServicioActualizarUsuario;
 import com.ceiba.usuario.servicio.ServicioCrearUsuario;
@@ -9,6 +15,8 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BeanServicio {
+
+    //=============   SERVICIOS USUARIO ===================
 
     @Bean
     public ServicioCrearUsuario servicioCrearUsuario(RepositorioUsuario repositorioUsuario) {
@@ -24,6 +32,33 @@ public class BeanServicio {
     public ServicioActualizarUsuario servicioActualizarUsuario(RepositorioUsuario repositorioUsuario) {
         return new ServicioActualizarUsuario(repositorioUsuario);
     }
-	
 
+    //======================================================
+
+    //=============   SERVICIOS TRABAJO DE GRADO ===================
+
+    @Bean
+    public ServicioCrearTrabajoDeGrado servicioCrearTrabajoDeGrado(RepositorioTrabajoDeGrado repositorioTrabajoDeGrado){
+        return new ServicioCrearTrabajoDeGrado(repositorioTrabajoDeGrado);
+    }
+
+    @Bean
+    public ServicioActualizarTrabajoDeGrado servicioActualizarTrabajoDeGrado(RepositorioTrabajoDeGrado repositorioTrabajoDeGrado){
+        return new ServicioActualizarTrabajoDeGrado(repositorioTrabajoDeGrado);
+    }
+
+    @Bean
+    public ServicioEliminarTrabajoDeGrado servicioEliminarTrabajoDeGrado(RepositorioTrabajoDeGrado repositorioTrabajoDeGrado){
+        return new ServicioEliminarTrabajoDeGrado(repositorioTrabajoDeGrado);
+    }
+
+    //======================================================
+
+    //=============   SERVICIOS CITA ===================
+
+    @Bean
+    public ServicioCrearCita servicioCrearCita(RepositorioCita repositorioCine){
+        return new ServicioCrearCita(repositorioCine);
+    }
+	
 }
--- trabajodegrado/src/main/resources/db/migration/DDL/V1.0__schema.sql
+++ trabajodegrado/src/main/resources/db/migration/DDL/V1.0__schema.sql
@@ -5,3 +5,23 @@ create table usuario (
  fecha_creacion datetime null,
  primary key (id)
 );
+
+create table puntotrabajodegrado (
+ id int(11) not null auto_increment,
+ nombre varchar(100) not null,
+ valor decimal(16,4) not null,
+ primary key (id)
+);
+
+create table trabajodegrado (
+ id int(11) not null auto_increment,
+ id_usuario int(11) not null,
+ nombre varchar(100) not null,
+ descripcion varchar(500),
+ estado varchar(100) not null,
+ valor decimal(16,4) not null,
+ fecha_confirmacion datetime,
+ primary key (id)
+);
+
+Alter table trabajodegrado ADD FOREIGN KEY (id_usuario) REFERENCES usuario(id);
