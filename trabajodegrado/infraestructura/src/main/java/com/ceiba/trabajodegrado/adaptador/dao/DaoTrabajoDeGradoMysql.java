cecef25376541c4a91cbb2041df92b3c4bfe8029
Miguel Angel Osorio Giron - Ceiba Software <miguel.osorio@ceiba.com.co>
2021-02-22 14:21:24 -0500

    Se crea api que retorna los trabajos de grados que aún no tienen una cita asociada

--- trabajodegrado/infraestructura/src/main/java/com/ceiba/trabajodegrado/adaptador/dao/DaoTrabajoDeGradoMysql.java
+++ trabajodegrado/infraestructura/src/main/java/com/ceiba/trabajodegrado/adaptador/dao/DaoTrabajoDeGradoMysql.java
@@ -4,6 +4,7 @@ import com.ceiba.infraestructura.jdbc.CustomNamedParameterJdbcTemplate;
 import com.ceiba.infraestructura.jdbc.sqlstatement.SqlStatement;
 import com.ceiba.trabajodegrado.modelo.dto.DtoTrabajoDeGrado;
 import com.ceiba.trabajodegrado.puerto.dao.DaoTrabajoDeGrado;
+import com.ceiba.usuario.adaptador.dao.MapeoUsuario;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.stereotype.Component;
 
@@ -22,6 +23,9 @@ public class DaoTrabajoDeGradoMysql implements DaoTrabajoDeGrado {
     @SqlStatement(namespace = NAMESPACE, value ="obtenerTrabajoDeGrado")
     private static String slqObtenerTg;
 
+    @SqlStatement(namespace = NAMESPACE, value ="listarTrabajosSinCitas")
+    private static String slqlistarTrabajosSinCitas;
+
     public DaoTrabajoDeGradoMysql(CustomNamedParameterJdbcTemplate customNamedParameterJdbcTemplate) {
         this.customNamedParameterJdbcTemplate = customNamedParameterJdbcTemplate;
     }
@@ -39,5 +43,10 @@ public class DaoTrabajoDeGradoMysql implements DaoTrabajoDeGrado {
         MapSqlParameterSource paramSource = new MapSqlParameterSource();
         paramSource.addValue("idTrabajoDeGrado", idTrabajoDeGradp);
         return this.customNamedParameterJdbcTemplate.getNamedParameterJdbcTemplate().query(slqObtenerTg, paramSource, new MapeoTrabajoDeGrado()).get(0);
+    }
+
+    @Override
+    public List<DtoTrabajoDeGrado> listarTrabajosSinCitas() {
+        return this.customNamedParameterJdbcTemplate.getNamedParameterJdbcTemplate().query(slqlistarTrabajosSinCitas, new MapeoTrabajoDeGrado());
     }
 }
